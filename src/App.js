14ea956e8300b29b527b0b7d42d1ffbf1f00332b
SunilRajoli <rajolisunilkumar@gmail.com>
2023-09-23 11:24:50 +0530

    myCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Signin from "./Components/Signin";
+import {Toaster} from 'react-hot-toast';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <>
+            <div>
+                <Toaster position="top-right" toastOptions={
+                    {
+                        duration: 5000,
+                        style: {
+                            fontFamily: 'monospace'
+                        }
+                    }
+                    
+                }></Toaster>
+                <BrowserRouter>
+                    <div>
+                        <Routes>
+                            <Route path="/" element={<Signin />}/>
+                            <Route path="/dash" element={<Dashboard />} />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </div>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Components/Dashboard/BarChart.jsx
@@ -0,0 +1,115 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import toast from "react-hot-toast";
+
+function BarChart() {
+    const [options, setOptions] = useState({
+        color: ["#b3ffd9", "#4d94ff"],
+        chart: {
+            background: "transparent",
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            curve: "smooth",
+        },
+        xaxis: {
+            categories: [],
+        },
+        legend: {
+            position: "top",
+        },
+        grid: {
+            show: false,
+        },
+    })
+
+    const [series, setSeries] = useState([
+        {
+            name: "Guest",
+            data: [],
+        },
+        {
+            name: "User",
+            data: [],
+        },
+    ])
+
+    useEffect(() => {
+        const price = [];
+        const stock = [];
+        const title = [];
+
+    axios.get("https://dummyjson.com/products")
+    .then((response) => {
+      const slicedProducts = response.data.products.slice(0, 4);
+      console.log(slicedProducts);
+      slicedProducts.map((item) => {
+        price.push(item.price);
+        stock.push(item.stock);
+        title.push(item.title);
+      });
+
+      setOptions({
+        color: ["#b3ffd9", "#4d94ff"],
+        chart: {
+          background: "transparent",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "smooth",
+        },
+        xaxis: {
+          categories: title,
+        },
+        legend: {
+          position: "top",
+        },
+        grid: {
+          show: false,
+        },
+      });
+      setSeries([
+        {
+          name: "Price",
+          data: price,
+        },
+        {
+          name: "Stock",
+          data: stock,
+        },
+      ]);
+    })
+    .catch((error) => {
+      console.error(error);
+      toast.error("Failed to fetch data.");
+    });
+    }, []);
+
+    return(
+        <div className="xl:w-[1000px] lg:w-[900px] max-xs:w-[85%] h-[359px] flex-shrink-0 rounded-[20px] border-[2px] border-[#e0e0e0] bg-white mt-[40.83px] lg:shadow-none max-xs:shadow-xl">
+            <div className="xl:w-[1000px] lg:w-[900px] h-[359px] ml-[40px] pt-[30px]">
+                <span className="w-[93px] text-black font-montserrat text-[18px] font-bold">
+                    Activities
+                </span> <br />
+                <span className="w-[93px] text-black font-montserrat text-[18px]">
+                    May - June 2021
+                </span>
+                
+                <Chart 
+                    options={options}
+                    series={series}
+                    type="bar"
+                    height="212px"
+                    width="921px"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default BarChart;
